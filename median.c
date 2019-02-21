3bb65835919e9292d809b58764289412862a253b
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-21 10:56:06 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,12 @@
+double(int a[],int n)
+[
+sort(n%2!=0)
+return(double)a[n/2];
+return0double(a[n-1]/2)+(a[n-2]/2.0);
+}
+int main()
+int a[]={12,34,67,89}
+int n=sizeof(a)/sizeoa(a[0])
+cout<<median="<<end;
+return 0;
+}
